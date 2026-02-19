52d674896ccb59e7a15f8bc3817eb546278a44ed
Manager2278 <badboybadasstoys@gmail.com>
2026-02-18 20:08:58 -0500

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,19 +1,19 @@
-<meta name='viewport' content='width=device-width, initial-scale=1'/>self.addEventListener("install", e => {
+const CACHE = "counter-coach-v1";
+
+self.addEventListener("install", e => {
   e.waitUntil(
-    caches.open("manager-tool-v3").then(cache => {
-      return cache.addAll([
+    caches.open(CACHE).then(cache =>
+      cache.addAll([
         "./",
         "./index.html",
         "./manifest.json"
-      ]);
-    })
+      ])
+    )
   );
 });
 
 self.addEventListener("fetch", e => {
   e.respondWith(
-    caches.match(e.request).then(response => {
-      return response || fetch(e.request);
-    })
+    caches.match(e.request).then(r => r || fetch(e.request))
   );
 });
